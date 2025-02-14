66fde22d790a916194405c824c14fa191a9b85ab
Hugo DELCROIX <91057582+HugoDELCROIX@users.noreply.github.com>
2025-02-14 17:44:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-// Fonction appelée lors du clic sur le bouton du fichier HTML
 function sendTest() {
-    // Remplacez par l'URL de votre webhook
     const webhookURL = "https://webhook.site/907da909-9a63-41a2-9f31-1e1a11de6ef9";
-
-    // Envoi d'une requête POST avec le message "test" au webhook
     fetch(webhookURL, {
         method: "POST",
         headers: {
